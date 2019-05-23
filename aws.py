91c15190d674478268c5a25e7e98da030883bba9
joel <joel@patriotsystems.com>
2019-05-23 21:24:07 +1200

    Update shadow reported state

--- aws.py
+++ aws.py
@@ -8,7 +8,7 @@ def get_date_time():
     time_now_utc = "{0:02d}{1:02d}{2:02d}".format(time_tuple[3], time_tuple[4], time_tuple[5])
     return (datestamp + "T" + time_now_utc + "Z")
 
-def getShadow(size=None):
+def makeShadowRequest(method = 'GET', body = '', contentType = 'application/json'):
     import awsiot_sign
     import json
     config = {}
@@ -21,24 +21,64 @@ def getShadow(size=None):
         config['access_key'], \
         config['secret_access_key'], \
         get_date_time(), \
-        region=config['region'])
+        region=config['region'], \
+        method=method, \
+        body=body)
 
     from microWebCli import MicroWebCli
     cli = MicroWebCli(auth = AuthIgnore())
     cli.Headers = signed['headers']
-    cli.Method = 'GET'
+    cli.Method = method
     cli.URL = 'https://' + signed['host'] + signed['uri']
-    cli.OpenRequest()
-    res = cli.GetResponse()
+    if body:
+        cli.OpenRequest(body, contentType)
+    else:
+        cli.OpenRequest()
+    return cli.GetResponse()
+
+def getShadow():
+    res = makeShadowRequest()
     if not res.IsSuccess():
-        return False
-    with open('shadow.json', 'wb+') as f:
+        return None
+    with open('shadowResponse.json', 'wb+') as f:
         while not res.IsClosed():
             b = res.ReadContent(1)
             if b is None:
                 break
             f.write(str(b, 'utf-8'))
-    return True
+    import json
+    shadowResponse = None
+    with open('shadowResponse.json', 'r') as f:
+        shadowResponse = json.load(f)
+    return shadowResponse
+
+def updateShadow():
+    shadowResponse = getShadow()
+    if shadowResponse is None:
+        return False
+    import json
+    if 'state' not in shadowResponse or \
+       'delta' not in shadowResponse['state']:
+        return None
+    if 'desired' not in shadowResponse['state']:
+        return False
+    activeShadow = shadowResponse['state']['desired']
+    with open('activeShadow.json', 'wb+') as f:
+        f.write(json.dumps(activeShadow))
+    body = json.dumps({ 'state': { 'reported': activeShadow } })
+    response = makeShadowRequest('POST', body)
+    success = response.IsSuccess()
+    response.Close()
+    return success
+
+def getActiveShadow():
+    try:
+        import json
+        with open('activeShadow.json', 'r') as f:
+            return json.load(f)
+    except:
+        return None
+
 
 class AuthIgnore:
     def Apply(self, microWebCli):
